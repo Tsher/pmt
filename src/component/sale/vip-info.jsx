e36875b241ab7296979c0b38737f77a0b72bb588
郝学亮 <haoxueliang@jindanlicai.com>
2016-01-26 14:37:29 +0800

    会员列表，促销列表，详细页

--- src/component/sale/vip-info.jsx
+++ src/component/sale/vip-info.jsx
@@ -13,207 +13,157 @@ import Col from 'antd/lib/col';
 import Row from 'antd/lib/row';
 import DatePicker from 'antd/lib/date-picker';
 import InputNumber from 'antd/lib/input-number';
-
 import Select from 'antd/lib/select';
 import Radio from 'antd/lib/radio';
-
 import { createHistory } from 'history';
 
 const FormItem = Form.Item;
 const Validator = Validation.Validator;
 const Option = Select.Option;
 const RadioGroup = Radio.Group;
-
 const FieldMixin = Validation.FieldMixin;
-
 const History = createHistory();
-
 const goBack = History.goBack;
 
 function cx(classNames) {
-  if (typeof classNames === 'object') {
-    return Object.keys(classNames).filter(function(className) {
-      return classNames[className];
-    }).join(' ');
-  } else {
-    return Array.prototype.join.call(arguments, ' ');
-  }
+    if (typeof classNames === 'object') {
+        return Object.keys(classNames).filter(function(className) {
+            return classNames[className];
+        }).join(' ');
+    } else {
+        return Array.prototype.join.call(arguments, ' ');
+    }
 }
 
 
-const msg_error = function(){
-  message.error('数据验证错误,请检查后提交')
+const msg_error = function() {
+    message.error('数据验证错误,请检查后提交')
 }
-const msg_success = function(){
-  message.success('数据提交成功，等待后台处理')
+const msg_success = function() {
+    message.success('数据提交成功，等待后台处理')
 }
 
 
 class SaleVipInfo extends React.Component{
-
   //mixins: [Validation.FieldMixin],
-
   constructor(props) {
-  	super(props);
-  	this.state = {
-      id : undefined, // 会员id
-      title : '会员信息',
-      vipName : undefined, // 昵称
-      vipLevel : undefined, // 会员级别
-      vipSex : undefined, // 会员性别
-      vipMobile : undefined, // 会员手机
-      vipState : undefined, // 会员状态
-      vipJoinDate : undefined, //注册日期
-      vipEmail : undefined, // 电子邮件
-      vipArea : undefined, // 行政区域
-      vipAddress : undefined , // 详细地址
-    };
-
-    this.handleResetPwd = this.handleResetPwd.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+      super(props);
+      this.state = {
+          Member_Code: undefined, // 会员id
+          title: '会员信息',
+          Member_SName: undefined, // 昵称
+          Member_Level: undefined, // 会员级别
+          Member_Sex: undefined, // 会员性别
+          Member_Phone: undefined, // 会员手机
+          Member_Status: undefined, // 会员状态
+          RegisterTime: undefined, //注册日期
+          Member_Email: undefined, // 电子邮件
+          SortNo: undefined, // 行政区域
+          Member_Address: undefined, // 详细地址
+      };
+      this.handleResetPwd = this.handleResetPwd.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  componentDidMount(){
-    var id = this.props.params.id;
-    // ajax 请求当前id的数据 ********************************
-    this.setState({
-      id : this.props.params.id
-    })
+  componentDidMount() {
+      var opts = {
+          Member_Code: this.props.params.id
+      }
+      _G.ajax({
+          url: config.__URL + config.sale.vip.info,
+          type: "get",
+          data: opts,
+          success: function(res) {
+              this.setState(Object.assign({}, res.Data))
+          }.bind(this)
+
+      })
+      this.setState({
+          Member_Code: this.props.params.id
+      })
   }
-
-
-  
 
   handleResetPwd(e) {
-    // 充值密码***********************************
-   
-    msg_success();
-    e.preventDefault();
+      // 重置密码***********************************
+      e.preventDefault();
+      var opts = {
+          Member_Code: this.props.params.id
+      }
+
+      _G.ajax({
+          url: config.__URL + config.sale.vip.resetPwd,
+          type: "get",
+          data: opts,
+          success: function(res) {
+              msg_success();
+          }.bind(this)
+
+      })
   }
 
   handleSubmit(e) {
-    //***********************************等待ajax提交数据 ******** 区分 新增 或者 编辑
-    e.preventDefault();
-    goBack();
-    
+      //***********************************等待ajax提交数据 ******** 区分 新增 或者 编辑
+      e.preventDefault();
+      goBack();
   }
 
-  
-
   render() {
-    const formData = this.state;
-
     return (
       <div className="m-form">
-        <div className="m-form-title">{this.state.title}</div>
-        <div className="m-form-con">
-      <Form horizontal>
-          <Row>
-            <Col span="8">
-                <FormItem
-                  label="会员ID："
-                  id="id"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}>
-                    <Input name="id" value={formData.id} disabled />
-                </FormItem>
-                <FormItem
-                  label="昵称："
-                  id="vipName"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipName" value={formData.vipName} disabled />
-                </FormItem>
-                <FormItem
-                  label="会员级别："
-                  id="vipLevel"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipLevel" value={formData.vipLevel} disabled />
-                </FormItem>
-                <FormItem
-                  label="性别："
-                  id="vipSex"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipSex" value={formData.vipSex} disabled />
-                </FormItem>
-                <FormItem
-                  label="手机："
-                  id="vipMobile"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipMobile" value={formData.vipMobile} disabled />
-                </FormItem>
-            </Col>
-            <Col span="8">
-                <FormItem
-                  label="会员状态："
-                  id="vipState"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}>
-                    <Input name="vipState" value={formData.vipState} disabled />
-                </FormItem>
-                
-                <FormItem
-                  label="注册日期："
-                  id="vipJoinDate"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipJoinDate" value={formData.vipJoinDate} disabled />
-                </FormItem>
-                <FormItem
-                  label="电子邮箱："
-                  id="vipEmail"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipEmail" value={formData.vipEmail} disabled />
-                </FormItem>
-                <FormItem
-                  label="行政区域："
-                  id="vipArea"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipArea" value={formData.vipArea} disabled />
-                </FormItem>
-                <FormItem
-                  label="地址："
-                  id="vipAddress"
-                  labelCol={{span: 8}}
-                  wrapperCol={{span: 12}}
-                  >
-                    <Input name="vipAddress" value={formData.vipAddress} disabled />
-                </FormItem>
-            </Col>
-            
-          </Row>
-          
-        
-      </Form>
-      </div>
-      <div className="m-form-btns">
-      <Row>
-        <Col span="4" offset="2">
-        <Button type="primary" onClick={this.handleResetPwd}>重置密码</Button>
-        &nbsp;&nbsp;&nbsp;&nbsp;
-        <Button type="primary" onClick={this.handleSubmit}>确定</Button>
-        </Col>
-      </Row>
-        
-      </div>
+          <div className="m-form-title">{this.state.title}</div>
+          <div className="m-form-con">
+              <Form horizontal>
+                  <Row>
+                      <Col span="8">
+                          <FormItem label="会员ID：" id="Member_Code" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Code" value={this.state.Member_Code} disabled />
+                          </FormItem>
+                          <FormItem label="昵称：" id="Member_SName" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_SName" value={this.state.Member_SName} disabled />
+                          </FormItem>
+                          <FormItem label="会员级别：" id="Member_Level" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Level" value={this.state.Member_Level} disabled />
+                          </FormItem>
+                          <FormItem label="性别：" id="Member_Sex" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Sex" value={this.state.Member_Sex} disabled />
+                          </FormItem>
+                          <FormItem label="手机：" id="Member_Phone" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Phone" value={this.state.Member_Phone} disabled />
+                          </FormItem>
+                      </Col>
+                      <Col span="8">
+                          <FormItem label="会员状态：" id="Member_Status" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Status" value={this.state.Member_Status} disabled />
+                          </FormItem>
+                          <FormItem label="注册日期：" id="RegisterTime" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="RegisterTime" value={this.state.RegisterTime} disabled />
+                          </FormItem>
+                          <FormItem label="电子邮箱：" id="Member_Email" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Email" value={this.state.Member_Email} disabled />
+                          </FormItem>
+                          <FormItem label="行政区域：" id="SortNo" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="SortNo" value={this.state.SortNo} disabled />
+                          </FormItem>
+                          <FormItem label="地址：" id="Member_Address" labelCol={{span: 8}} wrapperCol={{span: 12}}>
+                              <Input name="Member_Address" value={this.state.Member_Address} disabled />
+                          </FormItem>
+                      </Col>
+                  </Row>
+              </Form>
+          </div>
+          <div className="m-form-btns">
+              <Row>
+                  <Col span="4" offset="2">
+                      <Button type="primary" onClick={this.handleResetPwd}>重置密码</Button>
+                      &nbsp;&nbsp;&nbsp;&nbsp;
+                      <Button type="primary" onClick={this.handleSubmit}>确定</Button>
+                  </Col>
+              </Row>
+          </div>
       </div>
     );
   }
 };
-
-
 
 module.exports = {
   SaleVipInfo : SaleVipInfo
